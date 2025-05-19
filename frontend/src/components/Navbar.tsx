8184d28f318eb1698e4b6ef8d227197150709318
Mishulka <misailgromov@gmail.com>
2025-05-19 17:45:23 +0300

    init frontend app

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="p-4 text-2xl font-bold">
+      Navbar
+    </div>
+  )
+}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="p-4 text-2xl font-bold">
+      Домашняя страница
+    </div>
+  )
+}
